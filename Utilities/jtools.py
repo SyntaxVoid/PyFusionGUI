2af265743da22ec8d451ca59a73ddf40a27b9b38
SyntaxVoid <j.gresl12@gmail.com>
2017-06-20 12:13:55 -0700

    Updated utility functions.

--- Utilities/jtools.py
+++ Utilities/jtools.py
@@ -1,8 +1,20 @@
-### Utilities/jtools.py ###
-### John Gresl 6/19/2017 ###
+# Utilities/jtools.py #
+# John Gresl 6/19/2017 #
 import re
+import numpy as np
 
-def type_verify(var,typ):
+
+
+def complex_mag(z):
+    # Returns the magnitude sqrt(Re(z)^2 + Im(z)^2) of a numpy.complex number, z
+    return np.sqrt(z.real**2 + z.imag**2)
+
+def complex_mag_list(zz):
+    # Takes in an array of complex numbers and returns an array of their magnitudes
+    return [complex_mag(z) for z in zz]
+
+
+def type_verify(var, typ):
     return type(var) == typ
 
 
@@ -24,7 +36,7 @@ def shot_str_parser(s):
     # Example: An input of "1,6-9,12" will return [1,6,7,8,9,12]
     if not type_verify(s, str):
         raise TypeError("s must be type str. got type {}".format(type(s)))
-    out = [ ]
+    out = []
     segments = s.split(",")
     for segment in segments:
         # Verify each segment is either a single value or a range with one "-"
@@ -34,7 +46,31 @@ def shot_str_parser(s):
             if m1.match(segment):
                 out.append(int(segment))
             elif m2.match(segment):
-                out += range(int(segment.split("-")[0]),int(segment.split("-")[1])+1)
+                out += range(int(segment.split("-")[0]), int(segment.split("-")[1])+1)
         else:
             raise ValueError("Invalid string.")
     return out
+
+
+def squareish_grid(n, swapxy=False):
+    # Returns the grid closest to a square that will fit n elements
+    rows = 1
+    cols = 1
+    while rows*cols < n:
+        if rows == cols:
+            cols += 1
+        else:
+            rows += 1
+    return (rows, cols) if not swapxy else (cols, rows)
+
+
+def find_closest(arr, x):
+    m = abs(arr[0]-x)
+    x0 = arr[0]
+    n0 = 0
+    for n, item in enumerate(arr):
+        if abs(item-x) < m:
+            m = abs(item-x)
+            x0 = item
+            n0 = n
+    return n0, x0
