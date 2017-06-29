db913317f67bf8ed77f58af7b330f326aa3b60fe
SyntaxVoid <j.gresl12@gmail.com>
2017-06-28 18:48:54 -0700

    Debugging.

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -92,6 +92,7 @@ class Analysis:
         elif type(time_windows[0]) is not list:
             time_windows = list(itertools.repeat(time_windows[0], len(shots)))
         self.time_windows = time_windows
+        print("DEBUG3:", self.time_windows)
         self.input_data_iter = itertools.izip(self.shots, self.time_windows)
         self.device = device
         self.probes = probes
@@ -133,7 +134,7 @@ class Analysis:
     def get_mags(self, shot, probes):
         dev = pf.getDevice(self.device)
         time_window = self.time_windows[self.shots.index(shot)]
-        print("DEBUG2:", shot, probes, time_window)
+        print("DEBUG2:",shot, probes, time_window)
         return dev.acq.getdata(shot, probes).reduce_time(time_window)
 
     def get_stft(self, shot):
