21b6775393c4b040f8085f47335851de5f78291e
SyntaxVoid <j.gresl12@gmail.com>
2017-07-01 19:08:01 -0700

    Added pathnames to each important project directoy. Call like root_dir = PyFusionGUI.PROJECT_ROOT_DIR after importing PyFusionGUI.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
-import os
+import os.path
 
-# Here is where all of the important PyFusion file locations will be stored. They are automatically
-# imported when importing PyFusionGUI
-
-PYFUSIONGUI_DIR = os.path.dirname(__file__)
-ANALYSIS_DIR = os.path.join(PYFUSIONGUI_DIR, "Analysis")
-GUI_DIR = os.path.join(PYFUSIONGUI_DIR, "GUI")
-UTILITIES_DIR = os.path.join(PYFUSIONGUI_DIR, "Utilities")
+# Here is where all of the important PyFusion file locations will be stored.
+# They are automatically imported when importing PyFusionGUI. You must make
+# sure the PARENT directory of PyFusion is listed in your Python path to import.
 
 
+PROJECT_ROOT_DIR = os.path.dirname(__file__)
+ANALYSIS_DIR = os.path.join(PROJECT_ROOT_DIR, "Analysis")
+GUI_DIR = os.path.join(PROJECT_ROOT_DIR, "GUI")
+PYFUSION_DIR = os.path.join(PROJECT_ROOT_DIR, "pyfusion")
+UTILITIES_DIR = os.path.join(PROJECT_ROOT_DIR, "Utilities")
+PICKLE_SAVE_DIR = os.path.join(PROJECT_ROOT_DIR, "SavePicklesHere")
