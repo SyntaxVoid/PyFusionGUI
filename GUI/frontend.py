a864467b1eafe3eaaed5d97b099dcec4aa21eeb2
SyntaxVoid <j.gresl12@gmail.com>
2017-07-09 16:53:48 -0700

    .

--- GUI/frontend.py
+++ GUI/frontend.py
@@ -856,7 +856,7 @@ filter_items: EM_VMM_kappas'''
 
         if self.valid_values():
             self.using_analysis_var.set("Now clustering. Please wait.")
-            self.using_analysis_label.config(fg="yellow2")
+            self.using_analysis_label.config(fg="goldenrod2")
             win = ClusteringWindow(master=self.root)
             t = threading.Thread(target=callback)
             t.start()
