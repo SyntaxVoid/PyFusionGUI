65481d860b74267a13bee6978aede9d222e221dc
SyntaxVoid <j.gresl12@gmail.com>
2017-07-14 00:17:51 -0700

    Slurm work.

--- GUI/frontend.py
+++ GUI/frontend.py
@@ -47,6 +47,7 @@ class ClusteringWindow:
     def __init__(self, master, slurm_start_time=None, jobid=None, ANobj_restore=None):
         self.master = master
         self.root = tk.Toplevel(master=self.master)
+        self.root.geometry("500x500")
         self.root.resizable(height=False, width=False)
         self.message_frame = tk.Frame(master=self.root)
         self.message_frame.grid(row=0, column=0, sticky=tk.N)
@@ -98,6 +99,7 @@ class ClusteringWindow:
 
     def yes_cancel(self):
         subprocess.check_output("scancel {}".format(self.jobid), shell=True)
+        self.root.destroy()
         return
 
     def countdown(self):
